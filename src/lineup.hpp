bd62b360ea5cbc851b4a8b3911432392e70ed11f
nwright <nathanwrightbusiness@gmail.com>
2022-04-20 15:01:49 -0400

    finished arm lineup math

--- src/lineup.hpp
+++ src/lineup.hpp
@@ -32,30 +32,91 @@ lineup::~lineup() {
     delete tfListener;
 }
 
+// void lineup::callback(geometry_msgs::PointStamped inputPoint) {
+//     // do something
+//     std::string transformFrame = "odom";
+//     geometry_msgs::PointStamped point = tfBuffer.transform(inputPoint, "map");
+//     geometry_msgs::TransformStamped transOdomToMap = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+
+//     pub = n.advertise<geometry_msgs::Twist>("/stretch_diff_drive_controller/cmd_vel", 1000);
+
+//     tf2::Vector3 v(transOdomToMap.transform.translation.x, transOdomToMap.transform.translation.y, transOdomToMap.transform.translation.z);
+//     geometry_msgs::Twist rotate;
+//     rotate.angular.z = 0.5;
+//     ros::Rate loop_rate(10);
+//     while (ros::ok()) {
+//         tfPoint = tfBuffer.transform(inputPoint, "odom");
+//         ROS_INFO_STREAM(tfPoint);
+//         pub.publish(rotate);
+//         loop_rate.sleep();
+//     }
+
+//     geometry_msgs::Vector3 v1;
+//     tf2::Matrix3x3 m(tf2::Quaternion(transOdomToMap.transform.rotation.x, transOdomToMap.transform.rotation.y, transOdomToMap.transform.rotation.z, transOdomToMap.transform.rotation.w));
+//     m.getRPY(v1.x, v1.y, v1.z);
+//     rotate.angular.z = 0;
+//     pub.publish(rotate);
+//     ROS_INFO_STREAM("done\n"
+//                     << point << transOdomToMap);
+// }
+
+double getYaw(const geometry_msgs::Quaternion& input) {
+    tf2::Quaternion q;
+    tf2::fromMsg(input, q);
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+    return yaw;
+}
+
 void lineup::callback(geometry_msgs::PointStamped inputPoint) {
-    // do something
-    std::string transformFrame = "odom";
-    geometry_msgs::PointStamped point = tfBuffer.transform(inputPoint, "map");
-    geometry_msgs::TransformStamped transOdomToMap = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+    std::string targetFrame = "map";
+    std::string sourceFrame = "base_link";
+    geometry_msgs::PointStamped point = tfBuffer.transform(inputPoint, targetFrame);
+    geometry_msgs::TransformStamped transBaseLinkToMap = tfBuffer.lookupTransform(targetFrame, sourceFrame, ros::Time(0));
+    double x = transBaseLinkToMap.transform.translation.x - point.point.x;
+    double y = transBaseLinkToMap.transform.translation.y - point.point.y;
+    double h = sqrt(x * x + y * y);
+    ROS_INFO_STREAM(std::endl
+                    << "point: " << std::endl
+                    << "x: " << point.point.x << std::endl
+                    << "y: " << point.point.y);
+    ROS_INFO_STREAM(std::endl
+                    << "translation: " << std::endl
+                    << "x: " << transBaseLinkToMap.transform.translation.x << std::endl
+                    << "y: " << transBaseLinkToMap.transform.translation.y);
+    ROS_INFO_STREAM(std::endl
+                    << "translation - point: " << std::endl
+                    << "x: " << x << std::endl
+                    << "y: " << y << std::endl
+                    << "h: " << h);
+    double angle = acos(y / h);
+    // double angle = acos(y / h) - acos(transBaseLinkToMap.transform.translation.y / sqrt(transBaseLinkToMap.transform.translation.x * transBaseLinkToMap.transform.translation.x + transBaseLinkToMap.transform.translation.y * transBaseLinkToMap.transform.translation.y));
+    ROS_INFO_STREAM(std::endl
+                    << "acos: " << angle);
+    // ROS_INFO_STREAM(std::endl
+    //                 << "angle: " << angle);
 
-    pub = n.advertise<geometry_msgs::Twist>("/stretch_diff_drive_controller/cmd_vel", 1000);
+    double yaw = getYaw(transBaseLinkToMap.transform.rotation);
 
-    tf2::Vector3 v(transOdomToMap.transform.translation.x, transOdomToMap.transform.translation.y, transOdomToMap.transform.translation.z);
     geometry_msgs::Twist rotate;
     rotate.angular.z = 0.5;
     ros::Rate loop_rate(10);
-    while (ros::ok()) {
-        tfPoint = tfBuffer.transform(inputPoint, "odom");
-        ROS_INFO_STREAM(tfPoint);
+    pub = n.advertise<geometry_msgs::Twist>("/stretch_diff_drive_controller/cmd_vel", 1000);
+    // ROS_INFO_STREAM(point << std::endl
+    //                       << transBaseLinkToMap);
+    ROS_INFO_STREAM(fabs(yaw - angle));
+    while (ros::ok() && fabs(yaw - angle) > 0.10) {
+        transBaseLinkToMap = tfBuffer.lookupTransform(targetFrame, sourceFrame, ros::Time(0));
+        yaw = getYaw(transBaseLinkToMap.transform.rotation);
+        ROS_INFO_STREAM(fabs(yaw - angle));
+
+        if (yaw - angle < 0) {
+            rotate.angular.z = 0.5;
+        } else {
+            rotate.angular.z = -0.5;
+        }
         pub.publish(rotate);
         loop_rate.sleep();
     }
-
-    geometry_msgs::Vector3 v1;
-    tf2::Matrix3x3 m(tf2::Quaternion(transOdomToMap.transform.rotation.x, transOdomToMap.transform.rotation.y, transOdomToMap.transform.rotation.z, transOdomToMap.transform.rotation.w));
-    m.getRPY(v1.x, v1.y, v1.z);
-    rotate.angular.z = 0;
-    pub.publish(rotate);
-    ROS_INFO_STREAM("done\n"
-                    << point << transOdomToMap);
 }
